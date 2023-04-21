fc651c6cd5c919b5720def7dd3d53e52a07110a5
Pedro Henrique Fernandes <pedro007henrique41@gmail.com>
2023-04-21 23:59:50 +0100

    Project: example redis with C#

---
+++ Client.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace redis
+{
+    internal class Client
+    {
+        public string Cpf { get; private set; }
+        public string Name { get; private set; }
+        public string Email { get; private set; }
+
+        public Client(string cpf, string name, string email)
+        {
+            Cpf = cpf;
+            Name = name;
+            Email = email;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using ServiceStack.Redis;
+
+namespace redis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var host = "localhost:6379";
+            var key1 = "123456789";
+            var key2 = "987654321";
+
+            var client1 = new Client(key1, "John Doe", "john@doe.com");
+            var client2 = new Client(key2, "Jane Doe", "jane@doe.com");
+
+            using (var redis = new RedisClient(host))
+            {
+                redis.Set<Client>(key1, client1);
+                redis.Set<Client>(key2, client2);
+                var client1FromRedis = redis.Get<Client>(key1);
+                var client2FromRedis = redis.Get<Client>(key2);
+                Console.WriteLine("Client 1: {0}", client1FromRedis.Name);
+                Console.WriteLine("Client 2: {0}", client2FromRedis.Name);
+            }
+
+        }
+    }
+}
